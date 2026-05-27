3a1a747b8620d8da87e5746a291840531f2a4765
jeanlemignon <jeandavidbikie@gmail.com>
2026-05-27 13:16:00 +0200

    Initial commit

---
+++ src/main/java/com/example/simpleWebCommerceApp/SimpleWebCommerceApp.java
@@ -0,0 +1,13 @@
+package com.example.simpleWebCommerceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebCommerceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebCommerceApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleWebCommerceApp/SimpleWebCommerceAppTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleWebCommerceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebCommerceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
